d6fa46d54f069c14e112586563af5c1e63815fb9
HamimHossain <hamim.mgp@gmail.com>
2022-11-29 18:51:49 +0600

    user review post api added

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ async function run() {
         const userCollection = client.db('wildzyReview').collection('usersReview');
         const servicesCollection = client.db('wildzyReview').collection('services');
         
-        //---home page services---//
+        //---home page services api---//
         app.get('/services', async(req, res) => {
             const query = {};
             console.log(query)
@@ -31,26 +31,33 @@ async function run() {
             const services = await cursor.toArray();
             res.send(services);
         })
-        app.get('/services/:id', async(req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const service = await servicesCollection.findOne(query);
-            res.send(service);
-        })
 
-        //---all services---//
+        //---all services api---//
         app.get('/allServices', async(req, res) => {
             const query = {};
             const cursor = servicesCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
         })
+        
+        //---service dynamic with id api---//
         app.get('/allServices/:id', async(req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const service = await servicesCollection.findOne(query);
             res.send(service);
         })
+
+        //---user review api---//
+        app.post('/userReview', async(req, res) => {
+            const review = req.body;
+            const result = await userCollection.insertOne(review);
+            res.send(result);
+        })
+
+
+
+
 
     }
     finally{
